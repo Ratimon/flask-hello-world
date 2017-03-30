1081b7427a2e4aae1e6b6a5828f6e276741816f6
Rati <ratixoxo@gmail.com>
2017-03-30 05:32:48 +0000

    add url_for & getRequest

--- hello.py
+++ hello.py
@@ -1,12 +1,30 @@
 import os
 
-from flask import Flask
+from flask import Flask , request, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def show_url_for():
+    # display the url for a function
+    return url_for('show_user_profile', username='jorge')
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+    
+@app.route('/login', methods=['GET'])
+def login():
+    if request.values:
+        return "username is " + request.values['username']
+    else:
+        return '<form method="get" action="/login"><input type="text" name="username" /><p><button type="submit">Submit</button></form>'
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 5000)))
+    app.run(app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 5000))))
